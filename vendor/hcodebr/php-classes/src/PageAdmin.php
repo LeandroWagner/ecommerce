ce24ab80bb150dfad2a3dd2a01ab6ed6570c5b24
unknown <lsw_wagner@yahoo.com.br>
2020-01-05 02:19:56 -0300

    Tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,17 @@
+<?php
+namespace Hcode;
+
+CLass PageAdmin extends Page {
+
+//Construtor 
+	public function __construct($opts = array(),$tpl_dir = "/views/admin/" ) {
+		
+		parent::__construct($opts,$tpl_dir);
+	}
+
+}
+
+
+?>
+
+
